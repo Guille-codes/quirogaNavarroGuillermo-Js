c45c35ea7c4d742624ed6673b835c4605c56af7f
Guillermo Quiroga Navarro <73254008+Riquelme-code@users.noreply.github.com>
2022-09-03 19:49:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function multiplicador(capitalInicial, interes, meses){
+    let multiplicador = capitalInicial * interes * meses
+    return multiplicador
+}
+    
+let ingresar = prompt(" Calculadora de Interes Simple. ¿Desea calcular su plazo fijo? Responder con un Si o No.")
+
+while(ingresar.toLowerCase() == "si"){
+    let capitalInicial = prompt("Ingrese capital inicial. (En pesos)")
+    let interes = parseFloat(prompt("Ingrese el interes. (En decimal con . )"))
+    let meses = parseInt(prompt("Ingrese la cantidad de meses."))
+    
+    console.log("Su capital final es: " + multiplicador(capitalInicial, interes, meses))
+    alert("Su capital final es: " + multiplicador(capitalInicial, interes, meses))
+
+    ingresar = prompt("¿Quiere hacer otro calculo? Responder Si o No.")
+}
